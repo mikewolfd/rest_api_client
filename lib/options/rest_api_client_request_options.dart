7e120ccd6db1011278bc990572eaf8f456e471e7
Michael Deeb <michael.f.deeb@gmail.com>
2025-02-28 01:53:38 -0500

    refactor: added option override

--- lib/options/rest_api_client_request_options.dart
+++ lib/options/rest_api_client_request_options.dart
@@ -2,19 +2,25 @@ import 'package:dio/dio.dart';
 
 class RestApiClientRequestOptions {
   Map<String, dynamic>? headers;
+  Options? options;
   String? contentType;
   bool silentException;
 
   RestApiClientRequestOptions({
     this.headers,
+    this.options,
     this.contentType,
     this.silentException = false,
   });
 
   Options toOptions() {
-    return Options(
-      headers: headers,
-      contentType: contentType,
-    );
+    return options?.copyWith(
+          headers: headers,
+          contentType: contentType,
+        ) ??
+        Options(
+          headers: headers,
+          contentType: contentType,
+        );
   }
 }
